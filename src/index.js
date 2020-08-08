7b16d82fe59489aeefb71078de64974a357b67e7
LucianoBWille <00lucianobwille00@gmail.com>
2020-08-08 13:08:18 -0300

    Adição do react-router e do cadastro de video

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import CadastroVideo from './pages/cadastro/video'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/Cadastro/video' component={CadastroVideo} />
+      <Route path='/' component={App} exact />
+      <Route component={() => (<div><h1>Erro 404</h1><a href='/'>Home</a></div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
